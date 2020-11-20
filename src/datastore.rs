b8d97ca8408bad3e83f475ee418a144071c06eea
Isaac Clayton <slightknack@gmail.com>
2020-11-20 16:30:59 +0100

    made the ordering of the struct declarations in datastore more logical

--- src/datastore.rs
+++ src/datastore.rs
@@ -12,6 +12,66 @@ use crate::{
 // TODO: how to make it so the user does not have to have the whole history on-hahd
 // but can still work with and modify that which they do have?
 
+// TODO: should a datastore be storable?
+// Probably not.
+/// A datastore is a database of two parts:
+/// The first part is the content-addressed storage.
+/// This is on-disk, with a cache of commonly used items.
+/// The second part is a tree of identities.
+/// This is built out in-memory, from the relations contained from the content-addressed code.
+pub struct Datastore {
+    path:              PathBuf,
+    local_branch:      Branch,
+    other_branches:    Vec<Branch>,
+    // TODO: maybe blob type?
+    cached_addresses:  HashMap<Address, Vec<u8>>,
+}
+
+impl Datastore {
+    fn load(&self, address: &Address) -> Option<Storable> {
+        let serialized = self.cached_addresses.get(address)?;
+        let object = rmp_serde::from_slice(serialized).ok()?;
+        return Some(object);
+    }
+
+    fn store(&mut self, storable: &Storable) -> Option<Address> {
+        let serialized = rmp_serde::to_vec(storable).ok()?;
+        let address    = Address::new(&serialized);
+        // TODO: store address permanently?
+        self.cached_addresses.insert(address.clone(), serialized);
+        return Some(address);
+    }
+
+    // TODO: commit
+    pub fn update(&mut self, storable: &Storable) -> Option<Delta> {
+        // get the identity of the storable object
+        let identity = storable.identity();
+        // find the most current version of that identity on the current branch
+        let _head = self.load(&self.local_branch.head(&identity)?)?;
+        // calculate the delta between that version and this new one
+        // let delta: Delta = Delta::make(head, storable);
+        // calculate the delta address
+        // let delta_address = Version::new(delta);
+        // cache & store the delta permanently
+        // calculate the content address
+        let address = self.store(storable)?;
+        // cache the content
+        // update the current version of this identity on the current branch
+        self.local_branch.commit(&identity, &address)?;
+        // return the delta
+        todo!()
+    }
+
+    pub fn register(&mut self, _storable: &Storable) -> Option<()> {
+        // get the identity of the storable object
+        // walk the context chain to determine the validity and location of the object
+        // calculate the content address
+        // cache & store the base version permanently
+        // update the current version of this identity on the current branch
+        todo!()
+    }
+}
+
 // TODO: should branch be storable?
 // I think it fits the bill.
 /// Represents a single chain of versions across an entire datastore branch.
@@ -104,66 +164,6 @@ pub struct Delta {
 impl Delta {
     pub fn new(previous: &Storable, next: &Storable) -> Delta {
         // calculate the diffs and addresses
-        todo!()
-    }
-}
-
-// TODO: should a datastore be storable?
-// Probably not.
-/// A datastore is a database of two parts:
-/// The first part is the content-addressed storage.
-/// This is on-disk, with a cache of commonly used items.
-/// The second part is a tree of identities.
-/// This is built out in-memory, from the relations contained from the content-addressed code.
-pub struct Datastore {
-    path:              PathBuf,
-    local_branch:      Branch,
-    other_branches:    Vec<Branch>,
-    // TODO: maybe blob type?
-    cached_addresses:  HashMap<Address, Vec<u8>>,
-}
-
-impl Datastore {
-    fn load(&self, address: &Address) -> Option<Storable> {
-        let serialized = self.cached_addresses.get(address)?;
-        let object = rmp_serde::from_slice(serialized).ok()?;
-        return Some(object);
-    }
-
-    fn store(&mut self, storable: &Storable) -> Option<Address> {
-        let serialized = rmp_serde::to_vec(storable).ok()?;
-        let address    = Address::new(&serialized);
-        // TODO: store address permanently?
-        self.cached_addresses.insert(address.clone(), serialized);
-        return Some(address);
-    }
-
-    // TODO: commit
-    pub fn update(&mut self, storable: &Storable) -> Option<Delta> {
-        // get the identity of the storable object
-        let identity = storable.identity();
-        // find the most current version of that identity on the current branch
-        let _head = self.load(&self.local_branch.head(&identity)?)?;
-        // calculate the delta between that version and this new one
-        // let delta: Delta = Delta::make(head, storable);
-        // calculate the delta address
-        // let delta_address = Version::new(delta);
-        // cache & store the delta permanently
-        // calculate the content address
-        let address = self.store(storable)?;
-        // cache the content
-        // update the current version of this identity on the current branch
-        self.local_branch.commit(&identity, &address)?;
-        // return the delta
-        todo!()
-    }
-
-    pub fn register(&mut self, _storable: &Storable) -> Option<()> {
-        // get the identity of the storable object
-        // walk the context chain to determine the validity and location of the object
-        // calculate the content address
-        // cache & store the base version permanently
-        // update the current version of this identity on the current branch
         todo!()
     }
 }
