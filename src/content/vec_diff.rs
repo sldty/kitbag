372cebe74ae6ba4dfca04fa247574221a6db579a
Isaac Clayton <slightknack@gmail.com>
2020-11-21 17:54:03 +0100

    added vec diff. over the next few days, I need to read up on and implement the diffing algo.

---
+++ src/content/vec_diff.rs
@@ -0,0 +1,17 @@
+/// Calculates the diff of a vec of items.
+/// Uses the Myers diffing algorithm
+/// Applies pre-processing steps to increase efficiency,
+/// applies post-processing steps to increase readability.
+pub struct VecDiff<T> {
+
+}
+
+impl<T> VecDiff<T> {
+    pub fn make(prev: &Vec<T>, next: &Vec<T>) -> VecDiff<T> {
+        todo!()
+    }
+
+    pub fn apply(&self, prev: &Vec<T>) -> VecDiff<T> {
+        todo!()
+    }
+}
