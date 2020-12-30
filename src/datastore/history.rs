1237401eb5ebe74a71ca8b0f1b3723c62e9e2ae0
Isaac Clayton <slightknack@gmail.com>
2020-12-30 20:32:47 +0100

    started implementing keyspace

--- src/datastore/history.rs
+++ src/datastore/history.rs
@@ -1,4 +1,7 @@
-use std::collections::HashMap;
+use std::{
+    path::Path,
+    collections::HashMap,
+};
 use serde::{Serialize, Deserialize};
 use crate::Address;
 
@@ -13,7 +16,20 @@ pub struct History {
 }
 
 impl History {
-    // new
+    pub fn new() -> Result<History, String> {
+        Ok(History {
+            head: todo!(),
+            map:  HashMap::new(),
+        })
+    }
+
+    pub fn head(&self) -> Address { self.head.clone() }
+
+    pub fn contains_address(&self, address: &Address) -> bool {
+        self.map.contains_key(address)
+    }
+
+
     // head()
     // contains_address(Address)
     // get(Address)
--- src/datastore/key_space.rs
+++ src/datastore/key_space.rs
@@ -1,7 +1,9 @@
+use std::path::Path;
 use crate::{
     keys::KeyPublic,
-    handle::Identity,
+    handle::{hex, Identity},
     datastore::{
+        Storable,
         DiskMap,
         History
     }
@@ -12,10 +14,30 @@ use crate::{
 pub struct KeySpace {
     key_public: KeyPublic,
     /// Maps `Identity` -> `History`
-    delta_maps: DiskMap<History>,
+    histories: DiskMap<History>,
 }
 
 impl KeySpace {
+    // TODO: take a reference to KeyPublic?
+    pub fn new(key_public: KeyPublic, path: &Path) -> Result<KeySpace, String> {
+        Ok(KeySpace {
+            key_public,
+            histories: DiskMap::new(path)?,
+        })
+    }
+
+    pub fn contains_address(&self, identity: &Identity) -> bool {
+        self.histories.contains_key(&hex(&identity.bytes()))
+    }
+
+    pub fn get(&mut self, identity: &Identity) -> Result<History, String> {
+        self.histories.get(&hex(&identity.bytes()))
+    }
+
+    pub fn insert(&mut self, content: &Content) -> Result<Identity, String> {
+        todo!()
+    }
+
     // new
     // contains_identity(Identity)
     // get(Identity) -> Option<History>
